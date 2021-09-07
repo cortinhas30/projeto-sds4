f94b5fe18b85d9ceaa0473be46284ea9d91d7dc1
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2021-09-07 20:16:36 -0300

    atualizando nomes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
       <div className="container">
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-        <div className= "row px-3">
+        <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center">taxa de sucesso (%)</h5>
             <BarChart />
--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -12,7 +12,7 @@ const BarChart = () => {
 
     const mockData = {
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+            categories: ['Jolyne kujo', 'Dio Brando', 'Joseph Joestar', 'Giorno Giovanna', 'Jotaro Kuzo']
         },
         series: [
             {
